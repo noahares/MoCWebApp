4b15e15ae86a94f9c22f5874aac11558ba323feb
Noah Wahl <noah.ares@yahoo.de>
2019-07-08 12:24:14 +0200

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -177,14 +177,14 @@ function handleMotionEvent(event) {
                         break;
                 case -90:
                         tiltX = event.accelerationIncludingGravity.x;
-                        tiltY = event.accelerationIncludingGravity.y;
+                        tiltY = event.accelerationIncludingGravity.y * (-1);
                         break;
                 case 90:
                         tiltX = event.accelerationIncludingGravity.x * (-1);
-                        tiltY = event.accelerationIncludingGravity.y * (-1);
+                        tiltY = event.accelerationIncludingGravity.y;
                         break;
                 case 180:
-                        tiltX = event.accelerationIncludingGravity.x;
+                        tiltX = event.accelerationIncludingGravity.x ;
                         tiltY = event.accelerationIncludingGravity.y * (-1);
                         break;
         }
